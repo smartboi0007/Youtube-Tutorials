575cc6b08b9bc867beb3d81a4059e3ba555b322a
Smartboi <89623317+smartboi0007@users.noreply.github.com>
2021-09-13 11:26:34 +0530

    Update code.js

--- 24-7 host/code.js
+++ 24-7 host/code.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
 
 setInterval(async () => {
-  await fetch('Your glitch project live page URL here').then(console.log('Pinged!'))
+  await fetch('https://glitch.com/edit/#!/luxurious-tulip-ticket?path=server.js%3A12%3A23').then(console.log('Pinged!'))
 }, 240000)
